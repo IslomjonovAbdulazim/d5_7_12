fbcc0f3da1c8be91db73ad518209bd9e11ede8bb
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-03 17:31:44 +0500

    imp

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
   const SecondPage({super.key});
 
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
